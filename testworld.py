4ceb6cc07dd2e67613cfb9cceb972bfad6c7c4a8
Kaleb S <kalebfire@cox.net>
2021-01-24 20:02:36 -0800

    Add files via upload
    added some test code to generate a world

---
+++ testworld.py
@@ -0,0 +1,334 @@
+from __future__ import print_function
+from __future__ import division
+# ------------------------------------------------------------------------------------------------
+# Copyright (c) 2016 Microsoft Corporation
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ------------------------------------------------------------------------------------------------
+
+# Tutorial sample #5: Observations
+"""Perlin noise implementation."""
+
+
+###################################################################################################
+# Licensed under ISC
+# Sourced from: https://gist.github.com/eevee/26f547457522755cb1fb8739d0ea89a1
+from itertools import product
+import math
+import random
+import numpy as np
+
+def smoothstep(t):
+    """Smooth curve with a zero derivative at 0 and 1, making it useful for
+    interpolating.
+    """
+    return t * t * (3. - 2. * t)
+
+
+def lerp(t, a, b):
+    """Linear interpolation between a and b, given a fraction t."""
+    return a + t * (b - a)
+
+
+class PerlinNoiseFactory(object):
+    """Callable that produces Perlin noise for an arbitrary point in an
+    arbitrary number of dimensions.  The underlying grid is aligned with the
+    integers.
+    There is no limit to the coordinates used; new gradients are generated on
+    the fly as necessary.
+    """
+
+    def __init__(self, dimension, octaves=1, tile=(), unbias=False):
+        """Create a new Perlin noise factory in the given number of dimensions,
+        which should be an integer and at least 1.
+        More octaves create a foggier and more-detailed noise pattern.  More
+        than 4 octaves is rather excessive.
+        ``tile`` can be used to make a seamlessly tiling pattern.  For example:
+            pnf = PerlinNoiseFactory(2, tile=(0, 3))
+        This will produce noise that tiles every 3 units vertically, but never
+        tiles horizontally.
+        If ``unbias`` is true, the smoothstep function will be applied to the
+        output before returning it, to counteract some of Perlin noise's
+        significant bias towards the center of its output range.
+        """
+        self.dimension = dimension
+        self.octaves = octaves
+        self.tile = tile + (0,) * dimension
+        self.unbias = unbias
+
+        # For n dimensions, the range of Perlin noise is ±sqrt(n)/2; multiply
+        # by this to scale to ±1
+        self.scale_factor = 2 * dimension ** -0.5
+
+        self.gradient = {}
+
+    def _generate_gradient(self):
+        # Generate a random unit vector at each grid point -- this is the
+        # "gradient" vector, in that the grid tile slopes towards it
+
+        # 1 dimension is special, since the only unit vector is trivial;
+        # instead, use a slope between -1 and 1
+        if self.dimension == 1:
+            return (random.uniform(-1, 1),)
+
+        # Generate a random point on the surface of the unit n-hypersphere;
+        # this is the same as a random unit vector in n dimensions.  Thanks
+        # to: http://mathworld.wolfram.com/SpherePointPicking.html
+        # Pick n normal random variables with stddev 1
+        random_point = [random.gauss(0, 1) for _ in range(self.dimension)]
+        # Then scale the result to a unit vector
+        scale = sum(n * n for n in random_point) ** -0.5
+        return tuple(coord * scale for coord in random_point)
+
+    def get_plain_noise(self, *point):
+        """Get plain noise for a single point, without taking into account
+        either octaves or tiling.
+        """
+        if len(point) != self.dimension:
+            raise ValueError("Expected {} values, got {}".format(
+                self.dimension, len(point)))
+
+        # Build a list of the (min, max) bounds in each dimension
+        grid_coords = []
+        for coord in point:
+            min_coord = math.floor(coord)
+            max_coord = min_coord + 1
+            grid_coords.append((min_coord, max_coord))
+
+        # Compute the dot product of each gradient vector and the point's
+        # distance from the corresponding grid point.  This gives you each
+        # gradient's "influence" on the chosen point.
+        dots = []
+        for grid_point in product(*grid_coords):
+            if grid_point not in self.gradient:
+                self.gradient[grid_point] = self._generate_gradient()
+            gradient = self.gradient[grid_point]
+
+            dot = 0
+            for i in range(self.dimension):
+                dot += gradient[i] * (point[i] - grid_point[i])
+            dots.append(dot)
+
+        # Interpolate all those dot products together.  The interpolation is
+        # done with smoothstep to smooth out the slope as you pass from one
+        # grid cell into the next.
+        # Due to the way product() works, dot products are ordered such that
+        # the last dimension alternates: (..., min), (..., max), etc.  So we
+        # can interpolate adjacent pairs to "collapse" that last dimension.  Then
+        # the results will alternate in their second-to-last dimension, and so
+        # forth, until we only have a single value left.
+        dim = self.dimension
+        while len(dots) > 1:
+            dim -= 1
+            s = smoothstep(point[dim] - grid_coords[dim][0])
+
+            next_dots = []
+            while dots:
+                next_dots.append(lerp(s, dots.pop(0), dots.pop(0)))
+
+            dots = next_dots
+
+        return dots[0] * self.scale_factor
+
+    def __call__(self, *point):
+        """Get the value of this Perlin noise function at the given point.  The
+        number of values given should match the number of dimensions.
+        """
+        ret = 0
+        for o in range(self.octaves):
+            o2 = 1 << o
+            new_point = []
+            for i, coord in enumerate(point):
+                coord *= o2
+                if self.tile[i]:
+                    coord %= self.tile[i] * o2
+                new_point.append(coord)
+            ret += self.get_plain_noise(*new_point) / o2
+
+        # Need to scale n back down since adding all those extra octaves has
+        # probably expanded it beyond ±1
+        # 1 octave: ±1
+        # 2 octaves: ±1½
+        # 3 octaves: ±1¾
+        ret /= 2 - 2 ** (1 - self.octaves)
+
+        if self.unbias:
+            # The output of the plain Perlin noise algorithm has a fairly
+            # strong bias towards the center due to the central limit theorem
+            # -- in fact the top and bottom 1/8 virtually never happen.  That's
+            # a quarter of our entire output range!  If only we had a function
+            # in [0..1] that could introduce a bias towards the endpoints...
+            r = (ret + 1) / 2
+            # Doing it this many times is a completely made-up heuristic.
+            for _ in range(int(self.octaves / 2 + 0.5)):
+                r = smoothstep(r)
+            ret = r * 2 - 1
+
+        return ret
+#################################################################################################
+
+
+def getArray(size):
+    p = PerlinNoiseFactory(2,4)
+    a = np.array([[p(i/size,j/size) for j in range(size)] for i in range(size)])
+    a = np.abs((a*50).astype(int))
+    return a
+
+from builtins import range
+from past.utils import old_div
+import MalmoPython
+import os
+import sys
+import time
+import json
+
+if sys.version_info[0] == 2:
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+else:
+    import functools
+    print = functools.partial(print, flush=True)
+
+def Menger(blocktype):
+    a = getArray(101)
+    #draw solid chunk
+    genstring = ""
+    #now remove holes
+    for i in range(101):
+        for j in range(101):
+            genstring += drawLine(i-50,0,j-50,i-50,a[i,j]+5,j-50,blocktype)+ "\n"
+    return genstring
+
+def drawLine(x1, y1, z1, x2, y2, z2, blocktype):
+    return '<DrawLine x1="' + str(x1) + '" y1="' + str(y1) + '" z1="' + str(z1) + '" x2="' + str(x2) + '" y2="' + str(y2) + '" z2="' + str(z2) + '" type="' + blocktype + '"/>'
+    
+missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            
+              <About>
+                <Summary>Hello world!</Summary>
+              </About>
+              
+            <ServerSection>
+              <ServerInitialConditions>
+                <Time>
+                    <StartTime>1000</StartTime>
+                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                </Time>
+                <Weather>clear</Weather>
+              </ServerInitialConditions>
+              <ServerHandlers>
+                  <FlatWorldGenerator generatorString="3;;1;"/>
+                  <DrawingDecorator>
+                    ''' + Menger("stone") + '''
+                  </DrawingDecorator>
+                  <ServerQuitFromTimeUp timeLimitMs="30000"/>
+                  <ServerQuitWhenAnyAgentFinishes/>
+                </ServerHandlers>
+              </ServerSection>
+              
+              <AgentSection mode="Survival">
+                <Name>MalmoTutorialBot</Name>
+                <AgentStart>
+                    <Placement x="0.5" y="20.0" z="0.5" yaw="90"/>
+                    <Inventory>
+                        <InventoryItem slot="8" type="diamond_pickaxe"/>
+                    </Inventory>
+                </AgentStart>
+                <AgentHandlers>
+                  <ObservationFromFullStats/>
+                  <ObservationFromGrid>
+                      <Grid name="floor3x3">
+                        <min x="-1" y="-1" z="-1"/>
+                        <max x="1" y="-1" z="1"/>
+                      </Grid>
+                  </ObservationFromGrid>
+                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                  <InventoryCommands/>
+                  <AgentQuitFromTouchingBlockType>
+                      <Block type="diamond_block" />
+                  </AgentQuitFromTouchingBlockType>
+                </AgentHandlers>
+              </AgentSection>
+            </Mission>'''
+
+# Create default Malmo objects:
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:",e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print("Waiting for the mission to start ", end=' ')
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission running ", end=' ')
+
+# Possible solution for challenge set in tutorial_4.py:
+
+#agent_host.sendCommand("hotbar.9 1") #Press the hotbar key
+#agent_host.sendCommand("hotbar.9 0") #Release hotbar key - agent should now be holding diamond_pickaxe
+
+#agent_host.sendCommand("pitch 0.2") #Start looking downward slowly
+#time.sleep(1)                        #Wait a second until we are looking in roughly the right direction
+#agent_host.sendCommand("pitch 0")    #Stop tilting the camera
+#agent_host.sendCommand("move 1")     #And start running...
+#agent_host.sendCommand("attack 1")   #Whilst flailing our pickaxe!
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+    if world_state.number_of_observations_since_last_state > 0: # Have any observations come in?
+        msg = world_state.observations[-1].text                 # Yes, so get the text
+        observations = json.loads(msg)                          # and parse the JSON
+        grid = observations.get(u'floor3x3', 0)                 # and get the grid we asked for
+        # ADD SOME CODE HERE TO SAVE YOUR AGENT
+print()
+print("Mission ended")
+# Mission has ended.
