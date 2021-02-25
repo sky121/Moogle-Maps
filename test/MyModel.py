c133d9b17275f9a960ea13a2f0160c724b482aa8
Kaleb Sverdrup <ksverdru@uci.edu>
2021-02-24 19:46:59 -0800

    Fixed Model

--- test/MyModel.py
+++ test/MyModel.py
@@ -14,20 +14,23 @@ class MyModel(TorchModelV2, nn.Module):
         self.conv1 = nn.Conv2d(1, 16, kernel_size = 3)
         self.conv2 = nn.Conv2d(16, 32, kernel_size = 3)
 
-        self.policy_layer = nn.Linear(32*11*11+2, 4)
-        self.value_layer = nn.Linear(32*11*11+2, 1)
+
+        self.dense_layer = nn.Linear(32*11*11+2, 64)
+        self.policy_layer = nn.Linear(64, 4)
+        self.value_layer = nn.Linear(64, 1)
         
         self.distance = None
         self.value = None
 
     def forward(self, input_dict, state, seq_lens):
-        
+
+        #print(input_dict)
         x = input_dict['obs']
-        print(x)
-        print(x.size())
-        self.distance = x[:, :2]
-        x = x[:, 2:]
-        x = x.reshape(32, 1, 15, 15)
+        #print(x)
+        #print(x.size())
+        self.distance = x[:,:2]
+        x = x[:,2:]
+        x = x.reshape(x.shape[0],1,15, 15)
         x = x.type(torch.float32)
         
         x = F.relu(self.conv1(x))
@@ -35,7 +38,8 @@ class MyModel(TorchModelV2, nn.Module):
 
         x = x.flatten(start_dim=1)
         x = torch.cat((self.distance,x), 1)
-        
+
+        x = F.relu(self.dense_layer(x))
         policy = self.policy_layer(x)
         self.value = self.value_layer(x)
         
